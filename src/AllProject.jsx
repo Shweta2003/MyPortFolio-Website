586dc50115b0d13cb9ec5c75d307dcfe1c692582
Shweta Mandal <65994349+Shweta2003@users.noreply.github.com>
2024-05-03 23:41:32 +0530

    Update AllProject.jsx

--- src/AllProject.jsx
+++ src/AllProject.jsx
@@ -1,6 +1,8 @@
+import img from "./Assets/re.png"
+
 const AllProject = [
     {
-        ProjectImg:"https://github.com/Shweta2003/resume-builder_/assets/65994349/0b1f3f41-6c58-42c0-b226-ff01fd33ef73",
+        ProjectImg:{img},
         ProjectLink:"https://ai-jobkit.web.app/",
         ProjectName:"Resume Builder",
         ProjectTech:"React",
